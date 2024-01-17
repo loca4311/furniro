c0b372f2c434573c5c3cf8d8b64d73e8a7359990
loca4311 <andyone4311@gmail.com>
2024-01-17 17:19:30 +0100

    Added google authentification

---
+++ src/lib/components/Core/Authentication.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Link } from '$lib/components';
+	import { authHandlers } from '$lib/store';
+</script>
+
+<div>
+	<Link on:click={authHandlers.loginWithGoogle}>Auth with google</Link>
+</div>
--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,8 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { firebaseConfig } from '$lib/config';
-import { initializeApp, type FirebaseApp } from 'firebase/app';
+import { initializeApp, type FirebaseApp, getApps, deleteApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-const app: FirebaseApp = initializeApp(firebaseConfig);
+let firebaseApp: FirebaseApp;
+
+if (!getApps().length || !firebaseApp) {
+	firebaseApp = initializeApp(firebaseConfig);
+} else {
+	firebaseApp = getApps()[0];
+	deleteApp(firebaseApp);
+	firebaseApp = initializeApp(firebaseConfig);
+}
+
+export const db = getFirestore(firebaseApp);
+export const firebaseAuth = getAuth(firebaseApp);
+export const storage = getStorage(firebaseApp);
+
+export const googleProvider = new GoogleAuthProvider();
